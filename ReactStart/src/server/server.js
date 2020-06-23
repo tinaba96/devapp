81347250b1a4db679aeccec007574c17a7595cae
tinaba96 <tinaba178.96@gmail.com>
2020-06-23 17:02:44 +0900

    update

--- ReactStart/src/server/server.js
+++ ReactStart/src/server/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import path from 'path';
+import config from 'config';
+import {logger} from './logger';
 
 const app = express();
+
+const serverConfig = config.get('server');
 
 app.use(express.static(path.join('./', 'dist')));
 
 app.get('/api', (req, res) => {
-  res.send({api: 'test'});
+  logger.info('access from ${req.headers.host}');
+  res.send({data: 'test'});
 })
 
 app.get('*', function (req, res) {
   res.sendFile(path.join('./', 'dist', 'index.html'))
 })
 
-app.listen(3000, ()=> {
-  console.log('server running');
+app.listen(serverConfig.port, ()=> {
+  logger.info('server starting -> [port] ${serverConfig.port} [env] ${process.env.NODE_ENV}');
 })
