7fb14c85b8b22f283c54ff9d050f621ef6913fda
tinaba96 <tinaba178.96@gmail.com>
2020-07-28 23:59:49 +0900

    update

---
+++ PHP/blog/resources/views/archives/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width>
+  <meta http-equal="X-UA-Compatible" content="ie=edge">
+  <title>記事一覧</title>
+</head>
+<body>
+    <h1> 記事一覧 </h1>
+
+    <p>viewで生成しました。</p>
+</body>
+</html>
