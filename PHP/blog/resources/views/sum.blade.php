6df0a95e990d25f1f14fd9aa991b73e8e328ca27
tinaba96 <tinaba178.96@gmail.com>
2020-07-29 23:07:19 +0900

    update

---
+++ PHP/blog/resources/views/sum.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+   <h1>足し算</h1>
+   <p>{{ $x }} + {{ $y }} = {{ $x + $y }} </p>
+</body>
+</html>
