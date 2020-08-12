0779e30d0f01fa87e58e669fbe6e21c86c2c412c
tinaba96 <tinaba178.96@gmail.com>
2020-08-13 00:04:39 +0900

    update

--- PHP/todo/app/Http/Controllers/TaskController.php
+++ PHP/todo/app/Http/Controllers/TaskController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task; 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -11,9 +13,60 @@ class TaskController extends Controller
     {
         $folders = Folder::all();
 
+        $current_folder = Folder::find($id);
+        $tasks = $current_folder->tasks()->get();
+
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
+    public function showCreateForm(int $id)
+    {
+    return view('tasks/create', [
+        'folder_id' => $id
+    ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+  {
+    $current_folder = Folder::find($id);
+
+    $task = new Task();
+    $task->title = $request->title;
+    $task->due_date = $request->due_date;
+
+    $current_folder->tasks()->save($task);
+
+    return redirect()->route('tasks.index', [
+        'id' => $current_folder->id,
+    ]);
+   }
+    public function showEditForm(int $id, int $task_id)
+    {
+    $task = Task::find($task_id);
+
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
+    }
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+    // 1
+    $task = Task::find($task_id);
+
+    // 2
+    $task->title = $request->title;
+    $task->status = $request->status;
+    $task->due_date = $request->due_date;
+    $task->save();
+
+    // 3
+    return redirect()->route('tasks.index', [
+        'id' => $task->folder_id,
+    ]);
+    }
 }
+
+
