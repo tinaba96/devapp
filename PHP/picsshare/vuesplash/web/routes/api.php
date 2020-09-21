ab1f80f3adc152813175e461d49358f3066f12a0
tinaba96 <tinaba178.96@gmail.com>
2020-09-21 23:29:01 +0900

    update

--- PHP/picsshare/vuesplash/web/routes/api.php
+++ PHP/picsshare/vuesplash/web/routes/api.php
@@ -11,6 +11,8 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 // ログインユーザー
 Route::get('/user', fn() => Auth::user())->name('user');
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
 
 /*
 |--------------------------------------------------------------------------
