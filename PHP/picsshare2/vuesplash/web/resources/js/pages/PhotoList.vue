3999154a92f9f811b30a0d1ee7beb4df10cc757d
tinaba96 <tinaba178.96@gmail.com>
2022-02-16 22:47:12 +0900

    Finish layout and routes

---
+++ PHP/picsshare2/vuesplash/web/resources/js/pages/PhotoList.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+        class="grid__item"
+        v-for="photo in photos"
+        :key="photo.id"
+        :item="photo"
+        @like="onLikeClick"
+      />
+    </div>
+    <Pagination :current-page="currentPage" :last-page="lastPage" />
+  </div>
+</template>
+
+<script>
+import { OK } from '../util'
+import Photo from '../components/Photo.vue'
+import Pagination from '../components/Pagination.vue'
+
+export default {
+  props: {
+    page: {
+      type: Number,
+      required: false,
+      default: 1
+    }
+  },
+  components: {
+    Photo,
+    Pagination
+  },
+  data () {
+    return {
+      photos: [],
+      currentPage: 0,
+      lastPage: 0
+    }
+  },
+  methods: {
+    async fetchPhotos () {
+      const response = await axios.get(`/api/photos/?page=${this.page}`)
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      this.photos = response.data.data
+      this.currentPage = response.data.current_page
+      this.lastPage = response.data.last_page
+    }
+  },
+  onLikeClick ({ id, liked }) {
+    if (! this.$store.getters['auth/check']) {
+      alert('いいね機能を使うにはログインしてください。')
+      return false
+    }
+
+    if (liked) {
+      this.unlike(id)
+    } else {
+      this.like(id)
+    }
+  },
+  watch: {
+    $route: {
+      async handler () {
+        await this.fetchPhotos()
+      },
+      immediate: true
+    }
+  },
+  async like (id) {
+    const response = await axios.put(`/api/photos/${id}/like`)
+
+    if (response.status !== OK) {
+      this.$store.commit('error/setCode', response.status)
+      return false
+    }
+
+    this.photos = this.photos.map(photo => {
+      if (photo.id === response.data.photo_id) {
+        photo.likes_count += 1
+        photo.liked_by_user = true
+      }
+      return photo
+    })
+  },
+  async unlike (id) {
+    const response = await axios.delete(`/api/photos/${id}/like`)
+
+    if (response.status !== OK) {
+      this.$store.commit('error/setCode', response.status)
+      return false
+    }
+
+    this.photos = this.photos.map(photo => {
+      if (photo.id === response.data.photo_id) {
+        photo.likes_count -= 1
+        photo.liked_by_user = false
+      }
+      return photo
+    })
+  }
+}
+</script>
+
